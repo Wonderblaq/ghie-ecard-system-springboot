a89a1cfed97371cc9151144829e816fea20c3982
Wonderblaq <wondersarfo0@gmail.com>
2026-08-26 15:21:24 +0100

    Query methods sql command fix

--- src/main/java/com/registrations/GhIE_ecard/repositories/MemberRepository.java
+++ src/main/java/com/registrations/GhIE_ecard/repositories/MemberRepository.java
@@ -25,20 +25,20 @@ public interface MemberRepository extends JpaRepository<StudentMember, Long> {
     List<StudentMember> findByRegionIn(Collection<Regions> regions);
 
     // Casts the timestamp to a plain date so time elements (hours/mins/seconds) are ignored
-    @Query("SELECT s FROM StudentMember s WHERE CAST(s.emailSentAt AS date) = :date")
+    @Query("SELECT s FROM StudentMember s WHERE CAST(s.emailSentAt AS date)  >= :date ORDER BY s.emailSentAt DESC")
     List<StudentMember> findByEmailSentAtGreaterThanEqualOrderByEmailSentAtDesc(@Param("date") LocalDate date);
 
     // Multi-tenant version filtered by regions
-    @Query("SELECT s FROM StudentMember s WHERE CAST(s.emailSentAt AS date) = :date AND s.region IN :regions")
+    @Query("SELECT s FROM StudentMember s WHERE CAST(s.emailSentAt AS date) >= :date AND s.region IN :regions ORDER BY s.emailSentAt DESC")
     List<StudentMember> findByEmailSentAtGreaterThanEqualAndRegionInOrderByEmailSentAtDesc(@Param("date") LocalDate date,
                                                                                            @Param("regions") Collection<Regions> regions);
 
-    // Query method to display student members based on their registration dates
-    @Query("SELECT s FROM StudentMember s WHERE CAST(s.registrationDate AS date) = :date")
+    // Query method to display student members registered from a given date onward
+    @Query("SELECT s FROM StudentMember s WHERE CAST(s.registrationDate AS date) >= :date ORDER BY s.registrationDate DESC")
     List<StudentMember> findByRegistrationDateGreaterThanEqualOrderByRegistrationDateDesc(@Param("date") LocalDate date);
 
     // Multi-tenant version filtered by their assigned regions
-    @Query("SELECT s FROM StudentMember s WHERE CAST(s.registrationDate AS date) = :date AND s.region IN :regions")
+    @Query("SELECT s FROM StudentMember s WHERE CAST(s.registrationDate AS date) >= :date AND s.region IN :regions ORDER BY s.registrationDate DESC")
     List<StudentMember> findByRegistrationDateGreaterThanEqualAndRegionInOrderByRegistrationDateDesc(
             @Param("date") LocalDate date,
             @Param("regions") Set<Regions> regions
